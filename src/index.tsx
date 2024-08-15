461651c2c2c288bb218ffbfdd00beeb1bc6beaf3
sinizano77 <sinizano77@gmail.com>
2024-08-15 16:28:00 -0700

    Applied CSS normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'normalize.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StyleSheetManager } from 'styled-components';
