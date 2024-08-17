bcf4d7687912b7c17f1110bb82c9c5de04a9ced2
sinizano77 <sinizano77@gmail.com>
2024-08-16 19:55:30 -0700

    Applied prettier to Construction files

--- src/components/UnderConstruction/constructionDb.tsx
+++ src/components/UnderConstruction/constructionDb.tsx
@@ -1,8 +1,9 @@
 import gearIcon from "../../assets/icons/gear_icon.png";
 
 export const constructionData = {
-    title: "Under Construction!",
-    text: "While this site is still currently being developed, go checkout the general mockup in ",
-    gearIcon: gearIcon,
-    figma: "https://www.figma.com/proto/ClWCptgmh6z1MHClSwnEQQ/Personal-Website?page-id=0%3A1&node-id=1-2&viewport=264%2C321%2C0.43&t=TRmYn7ACdQ6TR4rV-1&scaling=scale-down-width&content-scaling=fixed"
-}
+  title: "Under Construction!",
+  text: "While this site is still currently being developed, go checkout the general mockup in ",
+  gearIcon: gearIcon,
+  figma:
+    "https://www.figma.com/proto/ClWCptgmh6z1MHClSwnEQQ/Personal-Website?page-id=0%3A1&node-id=1-2&viewport=264%2C321%2C0.43&t=TRmYn7ACdQ6TR4rV-1&scaling=scale-down-width&content-scaling=fixed",
+};
--- src/components/UnderConstruction/underConstruction.tsx
+++ src/components/UnderConstruction/underConstruction.tsx
@@ -5,98 +5,97 @@ import { constructionData } from "./constructionDb";
 import useMediaQuery from "../../hooks/useMediaQuery";
 
 interface GearProps {
-    margin: string;
-    size: string;
-    rotationDirection: string;
-};
+  margin: string;
+  size: string;
+  rotationDirection: string;
+}
 
 const StyledGear = styled.img<GearProps>`
-    max-width: ${(props) => props.size};
-    margin: ${(props) => props.margin};
-    animation: spin 5s linear 0s infinite ${(props) => props.rotationDirection};
+  max-width: ${(props) => props.size};
+  margin: ${(props) => props.margin};
+  animation: spin 5s linear 0s infinite ${(props) => props.rotationDirection};
 
-    @keyframes spin {
-        100% {
-            transform:rotate(360deg); 
-        } 
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
     }
+  }
 `;
 
 const StyledUnderConstruction = styled.section`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    width: 100%;
-    background-color: #909090;
-    color: white;
-    padding: 5vw 0 6vw 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  background-color: #909090;
+  color: white;
+  padding: 5vw 0 6vw 0;
 
-    h2 {
-        font-family: "JetBrainsMono";
-        @media (${theme.breakpoints.xs}) {
-            font-size: 7vw;
-        }
-
-        @media (${theme.breakpoints.md}) {
-            font-size: 3vw;
-        }
+  h2 {
+    font-family: "JetBrainsMono";
+    @media (${theme.breakpoints.xs}) {
+      font-size: 7vw;
     }
 
-    p {
-        font-family: "Orkney";
-        padding: 0 10vw;
-        text-align: center;
-        @media (${theme.breakpoints.xs}) {
-            font-size: 4.5vw;
-        }
-
-        @media (${theme.breakpoints.md}) {
-            font-size: 1.75vw;
-
-        }
+    @media (${theme.breakpoints.md}) {
+      font-size: 3vw;
     }
-    
-    a:link,
-    a:visited,
-    a:active {
-        color: #86C5FF;
+  }
+
+  p {
+    font-family: "Orkney";
+    padding: 0 10vw;
+    text-align: center;
+
+    @media (${theme.breakpoints.xs}) {
+      font-size: 4.5vw;
     }
+
+    @media (${theme.breakpoints.md}) {
+      font-size: 1.75vw;
+    }
+  }
+
+  a:link,
+  a:visited,
+  a:active {
+    color: #86c5ff;
+  }
 `;
 
 function UnderConstruction() {
-    const isMobile = useMediaQuery(`(${theme.breakpoints.md})`);
+  const isMobile = useMediaQuery(`(${theme.breakpoints.md})`);
 
-    return (
-      <StyledUnderConstruction>
-        <h2>{constructionData.title}</h2>
-        <div>
-            <StyledGear 
-                size={isMobile ? "5vw" : "20vw"}
-                margin="auto"
-                rotationDirection="normal" 
-                src={constructionData.gearIcon} 
-                alt="spinning gear icon"
-            />
-            <StyledGear 
-                size={isMobile ? "3vw" : "12vw"}
-                margin={isMobile ? "0 0 2.5vw -1vw" : "0 0 10vw -4vw"}
-                rotationDirection="reverse" 
-                src={constructionData.gearIcon} 
-                alt="spinning gear icon"
-            />
-        </div>
-        <p>{constructionData.text}
-            <a href={constructionData.figma} 
-            target="_blank" 
-            rel="external">
-                Figma
-            </a>
-            !
-        </p>
-      </StyledUnderConstruction>
-    );
+  return (
+    <StyledUnderConstruction>
+      <h2>{constructionData.title}</h2>
+      <div>
+        <StyledGear
+          size={isMobile ? "5vw" : "20vw"}
+          margin="auto"
+          rotationDirection="normal"
+          src={constructionData.gearIcon}
+          alt="spinning gear icon"
+        />
+        <StyledGear
+          size={isMobile ? "3vw" : "12vw"}
+          margin={isMobile ? "0 0 2.5vw -1vw" : "0 0 10vw -4vw"}
+          rotationDirection="reverse"
+          src={constructionData.gearIcon}
+          alt="spinning gear icon"
+        />
+      </div>
+      <p>
+        {constructionData.text}
+        <a href={constructionData.figma} target="_blank" rel="external">
+          Figma
+        </a>
+        !
+      </p>
+    </StyledUnderConstruction>
+  );
 }
 
 export default UnderConstruction;
