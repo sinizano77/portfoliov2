ac14a45b0cb283fdf6e13864abb6a062bf63e3b3
sinizano77 <sinizano77@gmail.com>
2024-08-22 14:42:38 -0700

    reenabled vendor prefixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/navbar";
 import Sidebar from "./components/Sidebar/sidebar";
 import About from "./components/About/about";
 import UnderConstruction from "./components/UnderConstruction/underConstruction";
+import { StyleSheetManager } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./global/GlobalStyle";
 
@@ -11,14 +12,16 @@ import { GlobalStyle } from "./global/GlobalStyle";
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Navbar />
-        <Sidebar />
-        <Hero />
-        <About />
-        <UnderConstruction />
-      </BrowserRouter>
+      <StyleSheetManager enableVendorPrefixes>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Navbar />
+          <Sidebar />
+          <Hero />
+          <About />
+          <UnderConstruction />
+        </BrowserRouter>
+      </StyleSheetManager>
     </>
   );
 }
